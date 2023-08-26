0a2526f064ef61987cfaca5670cfa9f1bd9f6f0d
Mokaclub <ismart.plane@gmail.com>
2023-08-26 19:45:02 +0200

    week two prep exercies

--- Week2/prep-exercises/2-experiments/index.js
+++ Week2/prep-exercises/2-experiments/index.js
@@ -2,7 +2,6 @@
 
 function runExperiment(sampleSize) {
   const valueCounts = [0, 0, 0, 0, 0, 0];
-
   // TODO
   // Write a for loop that iterates `sampleSize` times (sampleSize is a number).
   // In each loop iteration:
@@ -12,6 +11,11 @@ function runExperiment(sampleSize) {
   //    value from the previous step. Use the first element of `valueCounts`
   //    for keeping a count how many times the value 1 is thrown, the second
   //    element for value 2, etc.
+
+  for (let i = 0; i < sampleSize; i++) {
+    const randomValue = Math.floor(Math.random() * 6) + 1;
+    valueCounts[randomValue - 1] += 1;
+  }
 
   const results = [];
 
@@ -25,6 +29,11 @@ function runExperiment(sampleSize) {
   //    two decimals, e.g. '14.60'.
   // 3. Then push that string onto the `results` array.
 
+  for (let count of valueCounts) {
+    const percentage = (count / sampleSize) * 100;
+    results.push(percentage.toFixed(2));
+  }
+
   return results;
 }
 
@@ -36,11 +45,10 @@ function main() {
   // value of the `sampleSizes` array.
   // Log the results of each experiment as well as the experiment size to the
   // console.
-  // The expected output could look like this:
-  //
-  // [ '26.00', '17.00', '10.00', '19.00', '16.00', '12.00' ] 100
-  // [ '14.60', '17.10', '19.30', '15.50', '16.70', '16.80' ] 1000
-  // [ '16.71', '16.68', '16.69', '16.66', '16.67', '16.59' ] 1000000
+  for (let size of sampleSizes) {
+    const experimentResults = runExperiment(size);
+    console.log(experimentResults, size);
+  }
 }
 
 main();
